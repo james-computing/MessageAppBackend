f0c8fc4e9100221026be0948b2a6f8a20a2f78b3
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-10 20:00:00 -0300

    Create AuthController with an action for registering a user

---
+++ JWTAuth/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using JWTAuth.Dtos;
+using JWTAuth.Models;
+using JWTAuth.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuth.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class AuthController(IAuthService authService) : ControllerBase
+    {
+        // Create a user in the database
+        [HttpPost]
+        public async Task<ActionResult> RegisterAsync(UserDto userDto)
+        {
+            User? user = await authService.RegisterAsync(userDto);
+            if(user == null)
+            {
+                return BadRequest("User already exists.");
+            }
+
+            return Created();
+        }
+
+    }
+}
