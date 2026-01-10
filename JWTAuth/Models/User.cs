7122d789f793d23213514e56a9a6ea1b040faaed
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-10 17:46:26 -0300

    Create User model

---
+++ JWTAuth/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JWTAuth.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string PasswordHash { get; set; } = String.Empty;
+        public string Role { get; set; } = String.Empty;
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpirationTime { get; set; }
+    }
+}
