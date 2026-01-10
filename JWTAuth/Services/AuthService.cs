3b8089d3a48fadcfeafd6d594057592aeb8b4c03
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-10 19:43:10 -0300

    Create class AuthService, that will implement IAuthService
    Only create the class, but implement later.

---
+++ JWTAuth/Services/AuthService.cs
@@ -0,0 +1,25 @@
+﻿using JWTAuth.Dtos;
+using JWTAuth.Models;
+
+namespace JWTAuth.Services
+{
+    public class AuthService : IAuthService
+    {
+        public async Task<User?> RegisterAsync(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TokenDto?> LoginAsync(UserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        // The access token will expire after some time. This method gives a new access token if the refresh token didn't expire yet.
+        // If the refresh token is invalid, the user will have to login again.
+        public async Task<TokenDto?> RefreshTokenAsync(TokenDto oldTokenDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
