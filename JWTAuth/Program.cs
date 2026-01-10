b636dac55cfe192f7d5af31d669aec1fa98dd502
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-10 19:43:38 -0300

    Setup dependency injection for IAuthService

--- JWTAuth/Program.cs
+++ JWTAuth/Program.cs
@@ -1,3 +1,4 @@
+using JWTAuth.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
