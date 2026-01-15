6c9e356f1541dcd90e9051f04e19d934ccf8efa8
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-14 23:17:10 -0300

    Create table dbo.users in SQLServer database
    It stores user information. For simplicity, its refresh token and expiration time are stored in this table, but this may change in the future.

---
+++ JWTAuth/Data/SQL/createUsersTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE dbo.users
+(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	email NVARCHAR(320) NOT NULL, -- 320 is the maximum length an email can have
+	passwordhash NVARCHAR(500) NOT NULL,
+	username NVARCHAR(100) NOT NULL,
+	userrole NVARCHAR(50) NOT NULL,
+	refreshtoken NVARCHAR(500) NULL,
+	refreshtokenexpirationtime DATETIME NULL
+);
