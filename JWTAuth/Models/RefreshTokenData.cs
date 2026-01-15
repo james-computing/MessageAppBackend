be5f15dc6444d9b97525a7db4bb371fbac7b476f
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-14 23:24:08 -0300

    Create a new model to store the refresh token and its expiration time

---
+++ JWTAuth/Models/RefreshTokenData.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTAuth.Models
+{
+    public class RefreshTokenData
+    {
+        public required string RefreshToken { get; set; }
+        public required DateTime RefreshTokenExpirationTime { get; set; }
+    }
+}
