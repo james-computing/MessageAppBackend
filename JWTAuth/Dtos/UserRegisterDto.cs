305bdd2bec9487e2bb0added256b263a3e827a18
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-14 23:08:44 -0300

    Create dtos for user registration and login

---
+++ JWTAuth/Dtos/UserRegisterDto.cs
@@ -0,0 +1,11 @@
+﻿using JWTAuth.Roles;
+
+namespace JWTAuth.Dtos
+{
+    public class UserRegisterDto
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string Username { get; set; }
+    }
+}
