6386951ebcd3d8a7f1441bc87766db8375dba60c
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-14 23:31:41 -0300

    Create an endpoint to refresh the access token

--- JWTAuth/Controllers/AuthController.cs
+++ JWTAuth/Controllers/AuthController.cs
@@ -3,6 +3,8 @@ using JWTAuth.Models;
 using JWTAuth.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Security.Claims;
 
 namespace JWTAuth.Controllers
 {
@@ -31,6 +33,52 @@ namespace JWTAuth.Controllers
             if(token == null)
             {
                 return BadRequest();
+            }
+            return Ok(token);
+        }
+
+        // The access token will expire after some time. If the refresh token didn't expire yet,
+        // this action will give a JWT with a new access token and same refresh token.
+        // If the refresh token is invalid, the user will have to login again.
+        // The [Authorize] will be used without checking if the access token is outdated, since the purpose
+        // of the endpoint is to get a new access token.
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<TokenDto?>> RefreshAccessTokenAsync(string refreshToken)
+        {
+            // The refresh token should only be passed to this endpoint,
+            // to avoid interception, so it is a parameter of the request.
+            // The access token is always passed through the headers.
+            // The authorization schema will get the claims for the user
+            // that were obtained from the access token.
+            // We search in the claims for the user id.
+            string? userIdString = null;
+            foreach(ClaimsIdentity identity in HttpContext.User.Identities)
+            {
+                Claim? claim = identity.FindFirst(ClaimTypes.NameIdentifier);
+                if(claim != null)
+                {
+                    userIdString = claim.Value;
+                }
+            }
+
+            if (userIdString == null)
+            {
+                return Unauthorized();
+            }
+
+            int userId;
+            bool parsed = int.TryParse(userIdString, out userId);
+            if(parsed == false)
+            {
+                return Unauthorized();
+            }
+
+            // Request for a new token, with a new access token (but same refresh token)
+            TokenDto? token = await authService.RefreshAccessTokenAsync(userId, refreshToken);
+            if(token == null)
+            {
+                return Unauthorized();
             }
             return Ok(token);
         }
