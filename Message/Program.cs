1f4dd6387bb8e765b0669f348391a273a44d5544
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-03 11:21:44 -0300

    Configure authentication

--- Message/Program.cs
+++ Message/Program.cs
@@ -1,6 +1,8 @@
 using Message.Hubs;
 using Message.UserIdProvider;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,63 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+if (builder.Environment.IsProduction())
+{
+    // For K8s deployment.
+    // The JWT settings will be stored in a secret in K8s.
+    // This secret is copied to the app/secrets/jwt-settings.json file
+    builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("secrets/jwt-settings.json");
+}
+
+string? appSettingsTokenNullable = builder.Configuration.GetValue<string>("AppSettings:Token");
+string? appSettingsIssuerNullable = builder.Configuration.GetValue<string>("AppSettings:Issuer");
+string? appSettingsAudienceNullable = builder.Configuration.GetValue<string>("AppSettings:Audience");
+if (appSettingsTokenNullable == null || appSettingsIssuerNullable == null || appSettingsAudienceNullable == null)
+{
+    throw new Exception("Failed to get AppSettings token, issuer or audience.");
+}
+string appSettingsToken = appSettingsTokenNullable;
+string appSettingsIssuer = appSettingsIssuerNullable;
+string appSettingsAudience = appSettingsAudienceNullable;
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(
+        (JwtBearerOptions options) =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidIssuer = appSettingsIssuer,
+                ValidateAudience = true,
+                ValidAudience = appSettingsAudience,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(appSettingsToken)),
+                ValidateLifetime = true
+            };
+
+            // From https://learn.microsoft.com/en-us/aspnet/core/signalr/authn-and-authz?view=aspnetcore-10.0
+            //options.Authority = "Authority URL";
+
+            options.Events = new JwtBearerEvents()
+            {
+                OnMessageReceived = context =>
+                {
+                    var accessToken = context.Request.Query["access_token"];
+
+                    // If the request is for our hub
+                    var path = context.HttpContext.Request.Path;
+                    if (!string.IsNullOrEmpty(accessToken)
+                        && path.StartsWithSegments("/Message"))
+                    {
+                        // Read the token out of the query string
+                        context.Token = accessToken;
+                    }
+                    return Task.CompletedTask;
+                }
+            };
+        }
+    );
+
 
 builder.Services.AddSignalR();
 // To uniquely identify the user of SignalR
