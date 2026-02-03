b46527c450670a0f2139bd683738bdd7e1bf2b7d
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-03 12:24:49 -0300

    Create a PowerShell script to delete the message app cluster from K8s

---
+++ K8s/kubectl-delete-all.ps1
@@ -0,0 +1,21 @@
+kubectl delete -f mssql.yaml -n messageapp
+
+kubectl delete -f auth-service-deploy.yaml -n messageapp
+
+kubectl delete -f message-service-deploy.yaml -n messageapp
+
+kubectl delete -f gateway.yaml -n messageapp
+
+kubectl delete secret mssql -n messageapp
+kubectl delete secret jwt-settings -n messageapp
+kubectl delete secret db-settings -n messageapp
+
+kubectl delete pvc mssql-mssql-0 -n messageapp
+
+# Delete Kafka cluster
+kubectl -n kafka delete $(kubectl get strimzi -o name -n kafka)
+kubectl delete pvc -l strimzi.io/name=my-cluster-kafka -n kafka
+# Delete Strimzi cluster operator
+kubectl -n kafka delete -f strimzi.yaml
+# Delete kafka namespace
+kubectl delete namespace kafka
