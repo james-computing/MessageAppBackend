13ba063c09586f5d2957ba3e8f8a6c7668ee2ca1
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-03 12:26:12 -0300

    Make a console app to test the message app
    It can test the message app deployed to K8s.

---
+++ Client/Client.cs
@@ -0,0 +1,206 @@
+﻿using JWTAuth.Dtos;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Text.Json;
+
+namespace Client
+{
+    internal class Client
+    {
+        private readonly HubConnectionBuilder hubConnectionBuilder;
+        private HubConnection? connection;
+        private readonly UserRegisterDto _userRegisterDto;
+        private TokenDto? token;
+
+        private const string chatHubUrl = "http://messageapp/Message/";
+        private const string registerUrl = "http://messageapp/Auth/Register";
+        private const string loginUrl = "http://messageapp/Auth/Login";
+        private const string testAuthConnection = "http://messageapp/Auth/TestConnection";
+
+        private readonly JsonSerializerOptions jsonSerializerOptions;
+
+        public Client(UserRegisterDto userRegisterDto)
+        {
+            Console.WriteLine("Constructing client...");
+            _userRegisterDto = userRegisterDto;
+
+            hubConnectionBuilder = new HubConnectionBuilder();
+
+            jsonSerializerOptions = new JsonSerializerOptions();
+            jsonSerializerOptions.PropertyNameCaseInsensitive = true;
+        }
+
+        public async Task ConfigureConnection()
+        {
+            if (token == null)
+            {
+                return;
+            }
+
+            connection = hubConnectionBuilder
+                .WithUrl(chatHubUrl, options =>
+                {
+                    options.AccessTokenProvider = () => Task.FromResult<string?>(token.AccessToken);
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole();
+                    logging.SetMinimumLevel(LogLevel.Debug);
+                })
+                .Build();
+
+            // When the connection is closed, try to reconnect after some time
+            connection.Closed += OnConnectionClosed;
+
+            // When connection receives a message, print it
+            connection.On<string, string>("ReceiveMessageAsync", async (string senderId, string message) =>
+            {
+                Console.WriteLine($"{senderId}: {message}");
+            });
+
+            connection.On<string>("ReceiveErrorMessageAsync", async (string errorMessage) =>
+            {
+                Console.WriteLine($"Error: {errorMessage}");
+            });
+        }
+
+        private async Task OnConnectionClosed(Exception? error)
+        {
+            if(error != null)
+            {
+                Console.WriteLine($"error: {error.Message}");
+            }
+
+            if(connection != null)
+            {
+                /*
+                Console.WriteLine("Connection closed, restarting...");
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+                */
+                Console.WriteLine("Connection closed.");
+            }
+        }
+
+        public async Task<bool> TestConnectionToAuth()
+        {
+            HttpClient httpClient = new HttpClient();
+
+            // Test connection first
+            HttpResponseMessage responseMessageConnectionTest = await httpClient.GetAsync(testAuthConnection);
+            if (!responseMessageConnectionTest.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Failed to connect to Auth service.");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Successfully connected to Auth service.");
+                string responseConnectionTest = await responseMessageConnectionTest.Content.ReadAsStringAsync();
+                Console.WriteLine($"{responseConnectionTest}");
+                return true;
+            }
+        }
+
+        public async Task<bool> Register(UserRegisterDto userRegisterDto)
+        {
+            Console.WriteLine("Trying to register new user...");
+
+            HttpClient httpClient = new HttpClient();
+
+            string serializedJson = JsonSerializer.Serialize(userRegisterDto);
+            Console.WriteLine($"json to post {serializedJson}");
+            using StringContent jsonContent = new StringContent(serializedJson, Encoding.UTF8, "application/json");
+            HttpResponseMessage responseMessage = await httpClient.PostAsync(registerUrl, jsonContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                string response = await responseMessage.Content.ReadAsStringAsync();
+                Console.WriteLine($"response: {response}");
+                return true;
+            }
+
+            Console.WriteLine("Failed to register new user or error in server");
+            return false;
+        }
+
+        public async Task<bool> Login(UserLoginDto userLoginDto)
+        {
+            Console.WriteLine("Trying to log in...");
+
+            HttpClient httpClient = new HttpClient();
+
+            string serializedString = JsonSerializer.Serialize(userLoginDto);
+            Console.WriteLine($"json to post:\n{serializedString}\n");
+
+            using StringContent jsonContent = new(serializedString, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage responseMessage = await httpClient.PostAsync(loginUrl, jsonContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Login successful.");
+                string content = await responseMessage.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+                token = JsonSerializer.Deserialize<TokenDto>(content, jsonSerializerOptions);
+                if(token == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            Console.WriteLine($"Failed to log in. Status code: {responseMessage.StatusCode}");
+            return false;
+        }
+
+        public async Task<bool> TryToConnectToChatHub()
+        {
+            Console.WriteLine("Connecting to chat hub...");
+            if (connection == null)
+            {
+                return false;
+            }
+
+            // Try to connect
+            try
+            {
+                await connection.StartAsync();
+                Console.WriteLine("Connection started");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return false;
+        }
+
+        public async Task SendMessage(string receiverId, string message)
+        {
+            if (connection == null)
+            {
+                return;
+            }
+
+            try
+            {
+                // Invoke method SendMessage of ChatHub, with arguments user and message
+                Console.WriteLine("Sending message to server...");
+                await connection.InvokeAsync("SendMessageAsync", receiverId, message);
+                Console.WriteLine("Message sent.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public async Task StopAsync()
+        {
+            if(connection != null)
+            {
+                await connection.StopAsync();
+            }
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,83 @@
+﻿using JWTAuth.Dtos;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Client
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            UserRegisterDto userRegisterDto = new UserRegisterDto()
+            {
+                Email = "john@hotmail.com",
+                Password = "banana",
+                Username = "John",
+            };
+
+            Client client = new Client(userRegisterDto);
+
+            bool connectionSuccessful = await client.TestConnectionToAuth();
+            if(!connectionSuccessful)
+            {
+                return;
+            }
+
+            bool Registered = await client.Register(userRegisterDto);
+            if (Registered)
+            {
+                Console.WriteLine("Registered new user.");
+            }
+            else
+            {
+                Console.WriteLine("User already registered or error.");
+                Console.WriteLine("Write q to quit or anything else to proceed.");
+                string? quit = Console.ReadLine();
+                if(quit == "q")
+                {
+                    return;
+                }
+            }
+
+            UserLoginDto userLoginDto = new UserLoginDto()
+            {
+                Email = userRegisterDto.Email,
+                Password = userRegisterDto.Password,
+            };
+            bool loggedIn = await client.Login(userLoginDto);
+            if(!loggedIn)
+            {
+                return;
+            }
+
+            await client.ConfigureConnection();
+
+            bool connected = await client.TryToConnectToChatHub();
+            if (!connected)
+            {
+                return;
+            }
+
+            string? message = null;
+            bool chatting = true;
+            Console.WriteLine("Starting chat. Send q to quit.");
+            while (chatting)
+            {
+                //Console.Write("Write a message:");
+                message = Console.ReadLine();
+                if (!string.IsNullOrEmpty(message))
+                {
+                    if (message == "q")
+                    {
+                        chatting = false;
+                    }
+                    else
+                    {
+                        await client.SendMessage(userRegisterDto.Email, message);
+                    }
+                }
+            }
+
+            await client.StopAsync();
+        }
+    }
+}
