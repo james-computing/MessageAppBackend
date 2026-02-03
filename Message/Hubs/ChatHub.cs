ce3925e8dcf4bbe5ec9f75047665cffd65b69c91
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-03 11:13:09 -0300

    Create a Hub from SignalR called ChatHub
    SignalR is used for real-time communication. SignalR uses WebSockets when possible, but falls to other protocols if necessary. The Hub represents the connection of the server with its clients. The ChatHub is a strongly typed Hub, with IChatClient representing the methods of the SignalR client.

---
+++ Message/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Message.Kafka.Producer;
+
+/*
+    The Hub is transient, it can be disposed fast. For this reason, it can't be used for long tasks.
+    The Hub.Context is null in the constructor, as far as my tests show, only use it in the methods.
+*/
+
+namespace Message.Hubs
+{
+    [Authorize]
+    public class ChatHub : Hub<IChatClient>
+    {
+    }
+}
