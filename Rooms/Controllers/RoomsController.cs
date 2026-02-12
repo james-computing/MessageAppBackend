120c1e00d2bd6352a00a6a6c983121c98c370a1f
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-12 18:50:58 -0300

    Create a controller for the rooms service

---
+++ Rooms/Controllers/RoomsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rooms.Data;
+using Rooms.Roles;
+
+namespace Rooms.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    [Authorize]
+    public class RoomsController(IDataAccess dataAccess) : ControllerBase
+    {
+    }
+}
