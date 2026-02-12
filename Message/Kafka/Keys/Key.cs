234c732b7973abbebc0b7c6c9d1630af98ac42f1
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-11 22:23:06 -0300

    Use int ids

--- Message/Kafka/Keys/Key.cs
+++ Message/Kafka/Keys/Key.cs
@@ -4,8 +4,9 @@ namespace Message.Kafka.Keys
 {
     public class Key
     {
-        public required string SenderId { get; set; }
-        public required string ReceiverId { get; set; }
+        public required int SenderId { get; set; }
+        public required int ReceiverId { get; set; }
+        public required DateTime Time { get; set; }
 
         public override string ToString()
         {
