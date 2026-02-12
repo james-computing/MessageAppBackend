908518d6b5687414d83ea8fea122d7e203821994
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-12 18:18:09 -0300

    Inject IDataAccess into ConsumerOnBackground and pass it to the Consumer

--- KafkaConsumer/Kafka/Consumer/ConsumerOnBackground.cs
+++ KafkaConsumer/Kafka/Consumer/ConsumerOnBackground.cs
@@ -1,12 +1,14 @@
-﻿namespace KafkaConsumer.Kafka
+﻿using KafkaConsumer.Data;
+
+namespace KafkaConsumer.Kafka
 {
     public class ConsumerOnBackground : BackgroundService
     {
         private readonly IConsumer kafkaConsumer;
 
-        public ConsumerOnBackground(IConfiguration configuration)
+        public ConsumerOnBackground(IConfiguration configuration, IDataAccess dataAccess)
         {
-            kafkaConsumer = new Consumer(configuration);
+            kafkaConsumer = new Consumer(configuration, dataAccess);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
