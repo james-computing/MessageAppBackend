f735096b3cc1b0c13fca62e4ad44f539968cf023
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-12 21:22:10 -0300

    Make stored procedure to get the ids of all rooms the user is in

---
+++ SQL/stored procedures/usersrooms table/getUserRooms.sql
@@ -0,0 +1,24 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		James
+-- Create date: 12/02/2026
+-- Description:	get the ids of the rooms the user is in
+-- =============================================
+CREATE PROCEDURE dbo.getUserRooms
+	-- Add the parameters for the stored procedure here
+	@userid INT = NULL
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT roomid
+	FROM dbo.usersrooms
+	WHERE userid = @userid;
+END
+GO
