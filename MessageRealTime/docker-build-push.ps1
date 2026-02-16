41fae83f5a2b02512b64c3fcd3b859ae58229009
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-16 17:31:15 -0300

    Rename the container of the message service for real-time communication

--- MessageRealTime/docker-build-push.ps1
+++ MessageRealTime/docker-build-push.ps1
@@ -1,3 +1,3 @@
 # Execute this script from same folder
-docker build -f .\Dockerfile -t jmstoledo/message-service:v61 ..
-docker push jmstoledo/message-service:v61
+docker build -f .\Dockerfile -t jmstoledo/message-real-time-service:v1 ..
+docker push jmstoledo/message-real-time-service:v1
