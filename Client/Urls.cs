917f3756af0770fa81cdb2555141725c2ca8a05d
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-16 13:48:46 -0300

    Make a Urls struct to store the urls to be used by the client

---
+++ Client/Urls.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Client
+{
+    internal struct Urls
+    {
+        // Auth urls
+        public readonly string testAuthConnectionUrl;
+        public readonly string registerUrl;
+        public readonly string loginUrl;
+
+        // Message urls
+        public readonly string chatHubUrl;
+
+        // Rooms urls
+        public readonly string createRoomUrl;
+
+        public Urls(bool productionUrls)
+        {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+
+            if (productionUrls)
+            {
+                configurationBuilder.AddJsonFile("urls.Production.json");
+            }
+            else
+            {
+                configurationBuilder.AddJsonFile("urls.Development.json");
+            }
+
+            IConfiguration configuration = configurationBuilder.Build();
+
+            testAuthConnectionUrl = configuration.GetValue<string>("TestAuthConnectionUrl")
+                                        ?? throw new Exception("Failed to get TestAuthConnectionUrl");
+            registerUrl = configuration.GetValue<string>("RegisterUrl")
+                                        ?? throw new Exception("Failed to get RegisterUrl");
+            loginUrl = configuration.GetValue<string>("LoginUrl")
+                                        ?? throw new Exception("Failed to get LoginUrl");
+            chatHubUrl = configuration.GetValue<string>("ChatHubUrl")
+                                        ?? throw new Exception("Failed to get ChatHubUrl");
+            createRoomUrl = configuration.GetValue<string>("CreateRoomUrl")
+                                        ?? throw new Exception("Failed to get CreateRoomUrl");
+        }
+    }
+}
