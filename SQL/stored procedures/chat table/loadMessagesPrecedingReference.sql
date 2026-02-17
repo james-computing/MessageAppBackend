8980209136ffd4d8dec7630ce65247d6a652e590
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-17 16:14:21 -0300

    Create stored procedure to load messages that precede a reference message

---
+++ SQL/stored procedures/chat table/loadMessagesPrecedingReference.sql
@@ -0,0 +1,27 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		James
+-- Create date: 17/02/2026
+-- Description:	load messages that comes before a message reference
+-- =============================================
+CREATE PROCEDURE dbo.loadMessagesPrecedingReference
+	-- Add the parameters for the stored procedure here
+	@roomid INT = NULL,
+	@messageidreference INT = NULL,
+	@quantity INT = NULL
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT TOP(@quantity) id, senderid, message, time
+	FROM dbo.chat
+	WHERE roomid = @roomid AND id < @messageidreference
+	ORDER BY id DESC;
+END
+GO
