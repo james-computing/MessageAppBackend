f81e512d51b676d0c9a56f59c279ae64f852f575
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-17 17:07:44 -0300

    Create a dto to load messages before a reference message

---
+++ MessageREST/Dtos/LoadMessagesPrecedingReferenceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MessageREST.Dtos
+{
+    public class LoadMessagesPrecedingReferenceDto
+    {
+        public required int RoomId { get; set; }
+        public required int MessageIdReference { get; set; }
+        public required uint Quantity { get; set; }
+    }
+}
