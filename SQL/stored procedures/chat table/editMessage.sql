a42b9f3f6295b89e7e921a6ab730ec2866649f48
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-17 16:15:58 -0300

    Create stored procedure to replace a message by a new one

---
+++ SQL/stored procedures/chat table/editMessage.sql
@@ -0,0 +1,25 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		James
+-- Create date: 17/02/2026
+-- Description:	replace message content by a new one
+-- =============================================
+CREATE PROCEDURE dbo.editMessage
+	-- Add the parameters for the stored procedure here
+	@messageid INT = NULL,
+	@newmessage NVARCHAR(100) = NULL
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	UPDATE dbo.chat
+	SET message = @newmessage
+	WHERE id = @messageid;
+END
+GO
