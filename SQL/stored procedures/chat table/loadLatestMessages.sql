09ab04ac95577bc2ace7b3aec0260394306a3f97
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-17 16:14:40 -0300

    Create stored procedure to load latest messages

---
+++ SQL/stored procedures/chat table/loadLatestMessages.sql
@@ -0,0 +1,26 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		James
+-- Create date: 17/02/2026
+-- Description:	load latest messages
+-- =============================================
+CREATE PROCEDURE dbo.loadLatestMessages
+	-- Add the parameters for the stored procedure here
+	@roomid INT = NULL,
+	@quantity INT = NULL
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT TOP(@quantity) id, senderid, message, time
+	FROM dbo.chat
+	WHERE roomid = @roomid
+	ORDER BY id DESC;
+END
+GO
