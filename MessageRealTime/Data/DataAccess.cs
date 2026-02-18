a0e1631f12b316f47df4cdb36cf7e6c4561c8eef
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-17 21:15:54 -0300

    Create a method to save the message to the database

--- MessageRealTime/Data/DataAccess.cs
+++ MessageRealTime/Data/DataAccess.cs
@@ -11,6 +11,14 @@ namespace Message.Data
         private const string GET_USER_ROOMS_PROCEDURE = "dbo.getUserRooms";
         private const string GET_USER_ID_FROM_EMAIL_PROCEDURE = "dbo.getUserIdFromEmail";
 
+        // Messages table
+        private const string ROOMID_VARIABLE = "roomid";
+        private const string SENDERID_VARIABLE = "senderid";
+        private const string CONTENT_VARIABLE = "content";
+        private const string TIME_VARIABLE = "time";
+
+        private const string SAVE_MESSAGE_PROCEDURE = "dbo.saveMessage";
+
         public async Task<IEnumerable<int>> GetRoomsIdsAsync(int userId)
         {
             DynamicParameters parameters = new DynamicParameters();
@@ -39,6 +47,23 @@ namespace Message.Data
             );
 
             return userId;
+        }
+
+        public async Task<int> SaveMessageAsync(int roomId, int senderId, string content, DateTime time)
+        {
+            DynamicParameters parameters = new();
+            parameters.Add(ROOMID_VARIABLE, roomId);
+            parameters.Add(SENDERID_VARIABLE, senderId);
+            parameters.Add(CONTENT_VARIABLE, content);
+            parameters.Add(TIME_VARIABLE, time);
+
+            int messageId = await connection.QuerySingleAsync<int>
+                            (
+                                SAVE_MESSAGE_PROCEDURE,
+                                parameters,
+                                commandType: CommandType.StoredProcedure
+                            );
+            return messageId;
         }
     }
 }
--- MessageRealTime/Data/IDataAccess.cs
+++ MessageRealTime/Data/IDataAccess.cs
@@ -3,7 +3,7 @@
     public interface IDataAccess
     {
         public Task<IEnumerable<int>> GetRoomsIdsAsync(int userId);
-
         public Task<int> GetUserIdAsync(string userEmail);
+        public Task<int> SaveMessageAsync(int roomId, int senderId, string content, DateTime time);
     }
 }
