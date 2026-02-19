320de3e63dcd7ad58ea0bd17fa0de04ceac967a1
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-19 00:00:48 -0300

    Restore the Message class for the REST project

---
+++ REST/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace REST.Models
+{
+    public class Message
+    {
+        public required int Id { get; set; }
+        public required int RoomId { get; set; }
+        public required int SenderId { get; set; }
+        public required string Content { get; set; }
+        public required DateTime Time { get; set; }
+    }
+}
