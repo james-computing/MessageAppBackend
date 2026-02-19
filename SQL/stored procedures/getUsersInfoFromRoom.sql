68bbf1cbf0bcdf7dfac90c9f05cce142e748d9ab
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-19 15:16:59 -0300

    Create a stored procedure to get the information about the users from a room
    Getting information from other users must be taken with care. The id and username will be used for the client to be able to display the username of the sender of every message in the room. The message already have the id, so we need the username that corresponds to each id. It may be the case that this should be changed for something more secure.

---
+++ SQL/stored procedures/getUsersInfoFromRoom.sql
@@ -0,0 +1,24 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		James
+-- Create date: 19/02/2026
+-- Description:	get user info from room. It returns the ids and usernames.
+-- =============================================
+CREATE PROCEDURE dbo.getUsersInfoFromRoom
+	-- Add the parameters for the stored procedure here
+	@roomid INT = NULL
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT dbo.usersrooms.userid AS 'id', dbo.users.username AS 'username'
+	FROM dbo.usersrooms INNER JOIN dbo.users ON (dbo.usersrooms.userid = dbo.users.id)
+	WHERE dbo.usersrooms.roomid = @roomid;
+END
+GO
