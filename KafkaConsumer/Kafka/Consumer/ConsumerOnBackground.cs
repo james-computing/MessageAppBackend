50580fb6198f806b67ffb5236d0f1e676e5416c2
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-19 18:08:09 -0300

    Inject ISerializer to deserialize Kafka values

--- KafkaConsumer/Kafka/Consumer/ConsumerOnBackground.cs
+++ KafkaConsumer/Kafka/Consumer/ConsumerOnBackground.cs
@@ -1,12 +1,14 @@
-﻿namespace KafkaConsumer.Kafka
+﻿using KafkaConsumer.Kafka.Values;
+
+namespace KafkaConsumer.Kafka
 {
     public class ConsumerOnBackground : BackgroundService
     {
         private readonly IConsumer kafkaConsumer;
 
-        public ConsumerOnBackground(IConfiguration configuration)
+        public ConsumerOnBackground(IConfiguration configuration, ISerializer serializer)
         {
-            kafkaConsumer = new Consumer(configuration);
+            kafkaConsumer = new Consumer(configuration, serializer);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
