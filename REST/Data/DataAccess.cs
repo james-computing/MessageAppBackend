be648e5b7fb8fc6088f21c7186692defb3fb222c
jamessimeao <jamessimeao@outlook.com>
2026-02-23 21:56:56 -0300

    Pass the enum value to the database as a string
    Otherwise, it is interpreted as a number, but the database stores the values as strings

--- REST/Data/DataAccess.cs
+++ REST/Data/DataAccess.cs
@@ -170,7 +170,7 @@ namespace REST.Data
         {
             DynamicParameters parameters = new();
             parameters.Add(ROOMID_VARIABLE, roomId);
-            parameters.Add(ROLEINROOM_VARIABLE, roleInRoom);
+            parameters.Add(ROLEINROOM_VARIABLE, roleInRoom.ToString());
 
             await connection.ExecuteAsync
             (
