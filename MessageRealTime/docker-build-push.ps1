1026ecb38008a7a766bcc9efc8926ad27a974f35
jamessimeao <jamessimeao@outlook.com>
2026-02-24 20:52:40 -0300

    Update version

--- MessageRealTime/docker-build-push.ps1
+++ MessageRealTime/docker-build-push.ps1
@@ -1,3 +1,3 @@
 # Execute this script from same folder
-docker build -f .\Dockerfile -t jmstoledo/message-real-time-service:v1 ..
-docker push jmstoledo/message-real-time-service:v1
+docker build -f .\Dockerfile -t jmstoledo/message-real-time:v2 ..
+docker push jmstoledo/message-real-time:v2
